1b8bb92c37432293d154f5148745324d27f388d7
janper231 <jeancarloss659@gmail.com>
2022-06-28 22:54:02 -0500

    feat: update some challenges

---
+++ challenge16/index.js
@@ -0,0 +1,10 @@
+export default function decodeNumber(symbols) {
+  const dictionary = { '.': 1, ',': 5, ':': 10, ';': 50, '!': 100 };
+
+  return [...symbols].reduce((number, symbol, index, array) => {
+    const currentSymbolValue = dictionary[symbol];
+    const nextSymbolValue = dictionary[array[index + 1]];
+    const operation = currentSymbolValue < nextSymbolValue ? -1 : 1;
+    return number + currentSymbolValue * operation;
+  }, 0);
+};
